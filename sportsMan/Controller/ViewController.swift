400d0a8afbc08797af36ba895d7f33a2202eba74
KDAKANG <909623082@qq.com>
2023-06-10 18:24:48 +0800

    shopview

--- sportsMan/Controller/ViewController.swift
+++ sportsMan/Controller/ViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //the top txt
+    @IBOutlet weak var head: UILabel!
+    
     //the buttons on the bottom
     @IBOutlet weak var mainbutton: UIButton!
     @IBOutlet weak var shopbutton: UIButton!
@@ -27,6 +29,16 @@ class ViewController: UIViewController {
     //in mainpage
     
     //in shoppage
+    @IBOutlet var items: [UIView]!
+    @IBOutlet weak var block_food: UIView!
+    @IBOutlet weak var block_sport: UIView!
+    @IBOutlet weak var block_health: UIView!
+    @IBOutlet weak var block_drink: UIView!
+    @IBOutlet weak var msg: UIView!
+    @IBOutlet weak var foodbutton: UIButton!
+    @IBOutlet weak var drinkbutton: UIButton!
+    @IBOutlet weak var sportbutton: UIButton!
+    @IBOutlet weak var healthbutton: UIButton!
     
     //in datapage
     
@@ -35,15 +47,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //
         let gradientLayer  = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [UIColor.cyan.cgColor,UIColor.white.cgColor]
         gradientLayer.startPoint = CGPoint(x:0,y:0)
         gradientLayer.endPoint = CGPoint(x:0,y:0.39)
-        view.layer.insertSublayer(gradientLayer, at: 0)
+        mainpage.layer.insertSublayer(gradientLayer, at: 0)
         
-        //yourView.layer.borderWidth = 5.0
-        //yourView.layer.borderColor = UIColor.black.cgColor
+        //set black border for items in shop
+        for item in items{
+            item.layer.borderWidth = 2.0
+            item.layer.borderColor = UIColor.black.cgColor
+            item.layer.cornerRadius = 5
+            item.clipsToBounds = true
+        }
+        
+        msg.isHidden = true
+        
+        
+
         
 //        print(recordList)
 //        let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -76,6 +99,8 @@ class ViewController: UIViewController {
         shoppage.isHidden = true
         datapage.isHidden = true
         personpage.isHidden = true
+        
+        head.text = "主页"
     }
     
     @IBAction func shopPressed(_ sender: Any) {
@@ -88,6 +113,8 @@ class ViewController: UIViewController {
         shoppage.isHidden = false
         datapage.isHidden = true
         personpage.isHidden = true
+        
+        head.text = "生活商场"
     }
     
     @IBAction func dataPressed(_ sender: Any) {
@@ -100,6 +127,8 @@ class ViewController: UIViewController {
         shoppage.isHidden = true
         datapage.isHidden = false
         personpage.isHidden = true
+        
+        head.text = "锻炼记录"
     }
     
     @IBAction func personPressed(_ sender: Any) {
@@ -112,6 +141,106 @@ class ViewController: UIViewController {
         shoppage.isHidden = true
         datapage.isHidden = true
         personpage.isHidden = false
+        
+        head.text = "个人"
+    }
+    
+    @IBAction func foodPressed(_ sender: Any) {
+        foodbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        drinkbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        sportbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        healthbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        foodbutton.backgroundColor = UIColor(named:"shopbutton_chosen")
+        foodbutton.setTitleColor(UIColor.black, for: .normal)
+        drinkbutton.backgroundColor = UIColor.white
+        drinkbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        sportbutton.backgroundColor = UIColor.white
+        sportbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        healthbutton.backgroundColor = UIColor.white
+        healthbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        
+        block_food.isHidden = false
+        block_drink.isHidden = true
+        block_sport.isHidden = true
+        block_health.isHidden = true
+    }
+    
+    @IBAction func drinkPressed(_ sender: Any) {
+        foodbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        drinkbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        sportbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        healthbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        foodbutton.backgroundColor = UIColor.white
+        foodbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        drinkbutton.backgroundColor = UIColor(named:"shopbutton_chosen")
+        drinkbutton.setTitleColor(UIColor.black, for: .normal)
+        sportbutton.backgroundColor = UIColor.white
+        sportbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        healthbutton.backgroundColor = UIColor.white
+        healthbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        
+        block_food.isHidden = true
+        block_drink.isHidden = false
+        block_sport.isHidden = true
+        block_health.isHidden = true
+    }
+    
+    @IBAction func sportPressed(_ sender: Any) {
+        foodbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        drinkbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        sportbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        healthbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        foodbutton.backgroundColor = UIColor.white
+        foodbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        drinkbutton.backgroundColor = UIColor.white
+        drinkbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        sportbutton.backgroundColor = UIColor.white
+        sportbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        healthbutton.backgroundColor = UIColor.white
+        healthbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        
+        block_food.isHidden = true
+        block_drink.isHidden = true
+        block_sport.isHidden = false
+        block_health.isHidden = true
+    }
+    
+    @IBAction func healthPressed(_ sender: Any) {
+        foodbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        drinkbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        sportbutton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        healthbutton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        foodbutton.backgroundColor = UIColor.white
+        foodbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        drinkbutton.backgroundColor = UIColor.white
+        drinkbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        sportbutton.backgroundColor = UIColor.white
+        sportbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        healthbutton.backgroundColor = UIColor.white
+        healthbutton.setTitleColor(UIColor(named:"word_0"), for: .normal)
+        
+        block_food.isHidden = true
+        block_drink.isHidden = true
+        block_sport.isHidden = true
+        block_health.isHidden = false
+    }
+    
+    @IBAction func closemsg(_ sender: Any) {
+        msg.isHidden = true
+    }
+    
+    @IBAction func openmsg_dr_1(_ sender: Any) {
+        msg.isHidden = false
+        
+    }
+    
+    @IBAction func openmsg_dr_2(_ sender: Any) {
+        msg.isHidden = false
+        
     }
 }
 
